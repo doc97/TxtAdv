1b19338a45b493fed4cc238b7701f08e499fc172
Daniel Riissanen <daniel.riissanen@gmail.com>
2018-12-15 12:44:25 +0200

    Expose IO from AdvGame

--- TxtAdv/AdvGame.cpp
+++ TxtAdv/AdvGame.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 AdvGame::AdvGame(IO* io)
-    : m_prompt(io)
+    : m_io(io), m_prompt(m_io)
 {
 }
 
@@ -14,10 +14,16 @@ void AdvGame::Init()
 {
     m_prompt.SetPrompt("> ");
     m_response.AddHandler("exit", [this]() { this->Exit(); });
+    m_response.AddHandler("hello", [this]() { this->GetIO()->WriteLine("world"); });
 }
 
 void AdvGame::Update()
 {
     std::string input = m_prompt.PromptInput();
     m_response.HandleInput(input);
+}
+
+IO* AdvGame::GetIO() const
+{
+    return m_io;
 }
--- TxtAdv/AdvGame.h
+++ TxtAdv/AdvGame.h
@@ -13,8 +13,10 @@ public:
 
     void Init() override;
     void Update() override;
+    IO* GetIO() const;
 
 private:
+    IO* m_io;
     Prompt m_prompt;
     ResponseSystem m_response;
 };
