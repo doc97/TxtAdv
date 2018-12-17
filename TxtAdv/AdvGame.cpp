b09adccc1cdb26a41c51c913f2cefae89da7d809
Daniel Riissanen <daniel.riissanen@gmail.com>
2018-12-17 19:12:26 +0200

    Add StoryBranch to AdvGame
    Example story initialization added

--- TxtAdv/AdvGame.cpp
+++ TxtAdv/AdvGame.cpp
@@ -21,12 +21,58 @@ void AdvGame::Init()
         [](const std::string& input) { return input == "hello"; },
         [this]() { this->GetIO()->WriteLine("world"); }
     );
+
+    InitStory();
+}
+
+void AdvGame::InitStory()
+{
+    InitPointOne();
+    InitPointTwo();
+}
+
+void AdvGame::InitPointOne()
+{
+    std::vector<ResponseHandler> handlers;
+    handlers.emplace_back(
+        [](const std::string& input) { return input == "exit"; },
+        [this]() { this->Exit(); }
+    );
+    handlers.emplace_back(
+        [](const std::string& input) { return input == "start"; },
+        [this]() { this->StoryNext(); }
+    );
+    m_branch.AddPoint("Welcome to TxtAdv!", handlers);
+}
+
+void AdvGame::InitPointTwo()
+{
+    std::vector<ResponseHandler> handlers;
+    handlers.emplace_back(
+        [](const std::string& input) { return input == "exit"; },
+        [this]() { this->Exit(); }
+    );
+    m_branch.AddPoint("This is you: ...", handlers);
 }
 
 void AdvGame::Update()
 {
+    m_response.ClearHandlers();
+    m_response.AddHandlers(m_branch.GetHead()->GetHandlers());
+
+    m_io->WriteLine(m_branch.GetHead()->GetText());
     std::string input = m_prompt.PromptInput();
     m_response.HandleInput(input);
+}
+
+void AdvGame::StoryNext()
+{
+    m_branch.Next();
+}
+
+void AdvGame::StoryPrev()
+{
+    m_branch.Prev();
 }
 
 IO* AdvGame::GetIO() const
--- TxtAdv/AdvGame.h
+++ TxtAdv/AdvGame.h
@@ -4,6 +4,7 @@
 #include "IO.h"
 #include "Prompt.h"
 #include "ResponseSystem.h"
+#include "StoryBranch.h"
 
 class AdvGame : public Game
 {
@@ -13,11 +14,18 @@ public:
 
     void Init() override;
     void Update() override;
+    void StoryNext();
+    void StoryPrev();
     IO* GetIO() const;
 
 private:
     IO* m_io;
     Prompt m_prompt;
     ResponseSystem m_response;
+    StoryBranch m_branch;
+
+    void InitStory();
+    void InitPointOne();
+    void InitPointTwo();
 };
 
