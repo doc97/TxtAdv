8cf753943115477da38119d678ed2af7d6a84604
Daniel Riissanen <daniel.riissanen@gmail.com>
2018-12-18 19:31:37 +0200

    Add InputHandler interface

---
+++ TxtAdv/InputHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class InputHandler
+{
+public:
+    virtual ~InputHandler() {}
+    virtual void HandleInput(const std::string& input) = 0;
+    virtual bool Matches(const std::string& input) const = 0;
+};
+
--- TxtAdv/ResponseHandler.h
+++ TxtAdv/ResponseHandler.h
@@ -2,17 +2,18 @@
 
 #include <functional>
 #include <string>
+#include "InputHandler.h"
 #include "ResponseMatch.h"
 
-class ResponseHandler
+class ResponseHandler : public InputHandler
 {
 public:
     ResponseHandler(const std::function<ResponseMatch(const std::string&)>& matcher,
         const std::function<void(const ResponseMatch&)>& func);
     ~ResponseHandler();
 
-    void HandleInput(const std::string& input);
-    bool Matches(const std::string&) const;
+    void HandleInput(const std::string& input) override;
+    bool Matches(const std::string&) const override;
 private:
     std::function<ResponseMatch(const std::string&)> m_matcher;
     std::function<void(const ResponseMatch&)> m_func;
