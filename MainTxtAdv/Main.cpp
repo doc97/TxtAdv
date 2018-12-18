3365e37ff8148cc478b024539fa0a984e1acbd98
Daniel Riissanen <daniel.riissanen@gmail.com>
2018-12-18 21:25:52 +0200

    Remove example LUA code from Main.cpp

--- MainTxtAdv/Main.cpp
+++ MainTxtAdv/Main.cpp
@@ -1,75 +1,7 @@
-#include <stdio.h>
 #include <iostream>
-
-#ifdef __cplusplus
-#include <lua.hpp>
-#else
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-#endif
-
-#include "Callback.h"
 #include "LuaManager.h"
 
-void print_error(lua_State* L)
-{
-    const char* msg = lua_tostring(L, -1);
-    puts(msg);
-    lua_pop(L, 1);
-}
-
-void execute(const char* filename)
-{
-    lua_State* L = luaL_newstate();
-
-     // Make standard LUA libraries available in the Lua object
-    luaL_openlibs(L);
-
-    // Register C functions
-    lua_register(L, "hey", hey);
-
-    if (luaL_loadfile(L, filename) != LUA_OK)
-    {
-        print_error(L);
-        return;
-    }
-
-    // LUA_MULTRET => All return values are pushed onto the stack
-    if (lua_pcall(L, 0, LUA_MULTRET, 0) != LUA_OK)
-    {
-        print_error(L);
-        return;
-    }
-}
-
-void exec_fn(const char* filename, const char* funcname)
-{
-    lua_State* L = luaL_newstate();
-    luaL_openlibs(L);
-
-    if (luaL_dofile(L, filename) != LUA_OK)
-    {
-        print_error(L);
-        return;
-    }
-
-    // Push function onto the stack
-    lua_getglobal(L, funcname);
-    if (!lua_isfunction(L, -1))
-    {
-        // Pop it back manually
-        lua_pop(L, 1);
-        return;
-    }
-
-    // No parameters, no return values
-    // Pops stack automatically
-    if (lua_pcall(L, 0, 0, 0) != LUA_OK)
-        print_error(L);
-}
-
-int main()
+void exampleLua()
 {
     LuaManager manager;
 
@@ -81,4 +13,9 @@ int main()
         std::cout << err << std::endl;
 
     getchar();
+}
+
+int main()
+{
+    exampleLua();
 }
